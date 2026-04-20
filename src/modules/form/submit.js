df0c2be61e5199474aaa0cb492ecc1ee0089605f
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-04-20 19:37:33 -0300

    feat: implement date change handling and form submission validation

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector('form');
+const clientName = document.getElementById('client');
 const selectedDate = document.getElementById('date');
 
 // Date atual para formatar o imput
@@ -14,6 +15,42 @@ selectedDate.min = inputToday; // .min é um atributo do input do tipo date que 
 form.onsubmit = (event) => {
   // Previne o comportamento padrão de recarregar a página
   event.preventDefault();
-  console.log("Enviado");
+  
+  try {
+    // Recupera o nome do cliente
+    const name = clientName.value.trim();
+    
+
+    if (!name) {
+      return alert("Por favor, informe o nome do cliente");
+    }
+
+    // Recupera o horário selecionado
+    const hourSelected = document.querySelector(".hour-selected");
+    
+    if (!hourSelected) {
+      return alert("Por favor, selecione um horário");
+    }
+
+    // Recupera somente a hora
+    const [hour] = hourSelected.innerText.split(":");
+
+    // Inseriria a hora na data para criar um objeto Date completo
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    // Gera um ID
+    const id = new Date().getTime();
+
+    console.log({
+      id, 
+      name, 
+      when,
+    })
+
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento");
+    console.log(error);
+    
+  }
   
 }
